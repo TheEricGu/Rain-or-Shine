38822ca35b06a5018b01cdc2d2e1a48d7774557e
Eric <efg37@cornell.edu>
2020-12-04 16:21:54 -0500

    Updated db.py

--- db.py
+++ db.py
@@ -2,77 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-association_table = db.Table(
-  'association',
-  db.Model.metadata,
-  db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-  db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
-)
-
-# implement database model classes
-class Course(db.Model):
-  __tablename__ = 'course'
+class Outfit(db.Model):
+  __tablename__ = 'outfit'
   id = db.Column(db.Integer, primary_key=True)
-  code = db.Column(db.String, nullable=False)
-  name = db.Column(db.String, nullable=False)
-  assignments = db.relationship('Assignment', cascade='delete')
-  users = db.relationship('User', secondary=association_table, back_populates='courses')
-  instructors = []
-  students = []
+  gender = db.Column(db.String, nullable=False)
+  weather = db.Column(db.String, nullable=False)
+  min_temp = db.Column(db.Integer, nullable=False)
+  max_temp = db.Column(db.Integer, nullable=False)
  
   def __init__(self, **kwargs):
-    self.code = kwargs.get('code')
-    self.name = kwargs.get('name')
-    self.assignments = []
-    self.instructors = []
-    self.students = []
+    self.gender = kwargs.get('gender')
+    self.weather = kwargs.get('weather')
+    self.min_temp = kwargs.get('min_temp')
+    self.max_temp = kwargs.get('max_temp')
 
   def serialize(self):
     return {
       'id': self.id,
-      'code': self.code,
-      'name': self.name,
-      'assignments': self.assignments,
-      'instructors': self.instructors,
-      'students': self.students
-    }
-
-class Assignment(db.Model):
-  __tablename__ = 'assignment'
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String, nullable=False)
-  due_date = db.Column(db.Integer, nullable=False)
-  course = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
-
-  def __init__(self, **kwargs):
-    self.title = kwargs.get('title')
-    self.due_date = kwargs.get('due_date')
-    self.course = kwargs.get('course')
-
-  def serialize(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'due_date': self.due_date,
-      'course': self.course
-    }
-
-class User(db.Model):
-  __tablename__ = 'user'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, nullable=False)
-  netid = db.Column(db.String, nullable=False)
-  courses = db.relationship('Course', secondary=association_table, back_populates='users')
-
-  def __init__(self, **kwargs):
-    self.name = kwargs.get('name')
-    self.netid = kwargs.get('netid')
-    self.courses = []
-
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'netid': self.netid,
-      'courses' : self.courses
+      'gender': self.gender,
+      'weather': self.weather,
+      'min_temp': self.min_temp,
+      'max_temp': self.max_temp
     }
