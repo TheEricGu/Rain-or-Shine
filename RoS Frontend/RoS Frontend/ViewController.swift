b1a95110543f98cba1bfd15ab6f5d8249b155c5f
Alyssa Zhang <az354@cornell.edu>
2020-12-04 23:36:16 -0500

    added initial swift files

---
+++ RoS Frontend/RoS Frontend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoS Frontend
+//
+//  Created by Alyssa Zhang on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RoS Frontend/RoS FrontendTests/RoS_FrontendTests.swift
@@ -0,0 +1,33 @@
+//
+//  RoS_FrontendTests.swift
+//  RoS FrontendTests
+//
+//  Created by Alyssa Zhang on 12/4/20.
+//
+
+import XCTest
+@testable import RoS_Frontend
+
+class RoS_FrontendTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
